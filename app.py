f86917712a50c44389e0d93f04d8482dcf3765d6
TOsaland <thomas.osaland@gmail.com>
2019-01-11 17:23:28 +0100

    changed filenames and added required images

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-import pyvips
-from pyvips import Image
+
+from flask import Flask, send_file, make_response, render_template
+
+
+app = Flask(__name__)
 
 
 
-test = Image.new_from_file("dz_files/11/1_1.jpeg")
-test = test.draw_line([0, 255, 0], 0, 0, 100000, 110000)
 
-test.pngsave("testy.png")
+@app.route('/')
+def main():
+    return render_template("testOpenseadragon.html")
+
+@app.route('/<folder>/<level>/<tile>')
+def getTile(folder, level, tile):
+    return send_file(folder+"/"+level+"/"+tile)
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
 
 
 
