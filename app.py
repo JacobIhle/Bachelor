046b7861b4bf222c30a2ec5d0452a9adf826f09e
TOsaland <thomas.osaland@gmail.com>
2019-01-14 14:55:13 +0100

    many changes to loading images + functionality, eg. zooming

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-
 from flask import Flask, send_file, make_response, render_template
 
-
 app = Flask(__name__)
-
-
-
 
 @app.route('/')
 def main():
     return render_template("testOpenseadragon.html")
 
-@app.route('/<folder>/<level>/<tile>')
-def getTile(folder, level, tile):
-    return send_file(folder+"/"+level+"/"+tile)
+@app.route('/<root>/<imageID>/<dzFiles>/<level>/<tile>')
+def getTile(root ,imageID ,dzFiles, level, tile):
+    return send_file(root+"/"+imageID+"/"+dzFiles+"/"+level+"/"+tile)
+
+@app.route('/<root>/<imageID>/<file>')
+def getDzi(root, imageID, file):
+    return send_file(root+"/"+imageID+"/"+file)
 
 if __name__ == '__main__':
     app.run(port=5000)
---
+++ static/GUI.js
@@ -0,0 +1,67 @@
+var viewer;
+var imageID;
+var dziInfo;
+
+$(document).ready(function() {
+    addNonViewerHandlers();
+});
+
+function addNonViewerHandlers() {
+    $("#testButton").on("click", function () {
+        imageID = "dziFolder/H281-03/H281.dzi";
+        fetch(imageID)
+            .then(response => response.text())
+            .then(text => dziInfo);
+
+        open_slide(imageID);
+        addViewerHandlers();
+    })
+}
+
+function open_slide(url) {
+    if (viewer) {
+        // Never reuse an existing viewer to avoid a timer leak
+        // (OpenSeadragon issue #14)
+        viewer.close();
+        $("#display").text("");
+    }
+
+    viewer = new Seadragon.Viewer("display");
+
+    viewer.config.animationTime = 0.5;
+    viewer.config.blendTime = 0.1;
+    viewer.config.zoomPerScroll = 1.05;
+
+    viewer.clearControls();
+    viewer.openDzi(url, dziInfo);
+}
+
+function addViewerHandlers() {
+
+    $("#zoom4x").on("click", function () {
+        viewer.viewport.zoomTo(4);
+    });
+
+    $("#zoom10x").on("click", function () {
+        viewer.viewport.zoomTo(10);
+    });
+
+    $("#zoom20x").on("click", function () {
+        viewer.viewport.zoomTo(20);
+    });
+
+    $("#zoom40x").on("click", function () {
+        viewer.viewport.zoomTo(40);
+    });
+
+    viewer.add_animationfinish(function () {
+        $("#currentZoomLevel").html(viewer.viewport.getZoom(true));
+    });
+
+    viewer.add_open(function () {
+        viewer.viewport.zoomTo(0.6);
+    });
+
+}
+
+
