2f40da641f19adb15b54b068f98f2f91c9b60691
Jacob Ihle <jacob.ihle@gmail.com>
2019-01-23 13:54:54 +0100

    Very basic login now working, can't access index without logging in first

--- app.py
+++ app.py
@@ -1,18 +1,76 @@
 #LICENSE: https://github.com/openslide/openslide/blob/master/lgpl-2.1.txt
-import os
-from flask import Flask, send_file, render_template, send_from_directory
-import openslide
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask import Flask, send_file, render_template, send_from_directory, redirect, request
 from openslide.deepzoom import DeepZoomGenerator
+from flask_sqlalchemy import SQLAlchemy
 from io import BytesIO
+import os
+import openslide
 
 image = None
 deepZoomGen = None
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "notSecure"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Moonkee1@localhost/flasklogintest"
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+## UserMixin contains the standard methods so we dont have to implement it our self
+class User(UserMixin, db.Model):
+    id = db.Column("UserID", db.Integer, primary_key=True)
+    username = db.Column("Username", db.Unicode)
+    password = db.Column("Password", db.Integer)
 
 
+@login_manager.user_loader
+def user_login(UserID):
+    return User.query.get(int(UserID))
+
+
+@app.route("/logout")
+@login_required
+def Logout():
+    logout_user()
+    return "You are now logged out"
+
+
+@app.route("/home")
+@login_required
+def home():
+    return "The current user is " + current_user.username
+
+
+@app.route("/password")
+@login_required
+def password():
+    return "The password is: " + str(current_user.password)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        pswrd = User.query.filter_by(password=password).first()
+        # Probably not the best way to do it
+        # TODO: Find the correct way
+        if user and pswrd is not None:
+            if user == pswrd:
+                login_user(user)
+                return render_template("index.html")
+    return render_template("login.html", user=user_login(1), )
+
+
+@login_manager.unauthorized_handler
+def CatchNotLoggedIn():
+    return redirect("/login")
 
 @app.route('/')
+@login_required
 def Main():
     return render_template("index.html")
 
@@ -62,9 +120,3 @@ def serve_pil_image(pil_img):
 
 if __name__ == '__main__':
     app.run(port=5000, threaded=True)
-
-
-
-
-
-
---
+++ user.py
@@ -0,0 +1,21 @@
+def User(parameter_list):
+
+    user = "jacobi"
+
+    def is_authenticated(self):
+        # TODO: Add check for authetication
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        # TODO: Add check for if user is anonymous
+        return False
+
+    def get_id(self):
+        return self.user
+
+
+
+    
