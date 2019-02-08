0ceb58b73bf9775c14c07e64f7dd0b3016aeaf05
TOsaland <35271125+TOsaland@users.noreply.github.com>
2019-02-08 10:34:16 +0100

    This file was a draft and is now obsolete.

--- getImagePathsTest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-
-for year in os.listdir("../../../../prosjekt/Histology/bladder_cancer_images"):
-    if os.path.isdir("../../../../prosjekt/Histology/bladder_cancer_images/"+year):
-        for filename in os.listdir("../../../../prosjekt/Histology/bladder_cancer_images/"+year):
-            print(year+"/"+filename)
