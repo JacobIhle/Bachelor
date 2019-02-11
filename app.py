b87145af6f5db87849f5efb795effc8f73f3cd32
TOsaland <35271125+TOsaland@users.noreply.github.com>
2019-02-11 09:51:49 +0100

    Merged the refactor stuff from dev to master

--- app.py
+++ app.py
@@ -4,40 +4,31 @@ from flask import Flask, send_file, render_template, send_from_directory, redire
 from werkzeug.security import generate_password_hash, check_password_hash
 from openslide.deepzoom import DeepZoomGenerator
 from flask_sqlalchemy import SQLAlchemy
-from time import gmtime, strftime
-from io import BytesIO
+import os
 import customLogger
 import openslide
-import logging
-import os
+import HelperClass
+
 
 allAvailableImages = {}
+
 dateTime = customLogger.DateTime()
 logger = customLogger.StartLogging()
-
-# Creates a new logging file for this session
-filename = str("logging\\" + str(strftime("%Y_%m_%d %H.%M.%S", gmtime())).replace(" ", "_") + ".txt")
-logging.basicConfig(filename=filename, level=logging.WARNING)
 
 image = None
 deepZoomGen = None
 
-with open("Login.txt", 'r') as f:
-    dbUser, dbPassword = f.readline().split("|")
-    f.close()
-
+dateTime = customLogger.DateTime()
+logger = customLogger.StartLogging()
 
 app = Flask(__name__)
-## TODO: Create a random secure hash
-app.config["SECRET_KEY"] = "notSecure"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://%s:%s@mysql2@ux.uis.no/dbthomaso" % (dbUser, dbPassword)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-logging.basicConfig(filename="logg.txt", level=logging.WARNING)
+HelperClass.ConfigureApp(app)
 
 db = SQLAlchemy(app)
+db.create_all()
+
 login_manager = LoginManager()
 login_manager.init_app(app)
-db.create_all()
 
 
 @app.route('/')
@@ -45,7 +36,6 @@ db.create_all()
 def Main():
     AvailableImages = GetAvailableImages()
     return render_template("index.html", images=AvailableImages)
-
 
 @app.route('/images/<filename>')
 def LoadControlImages(filename):
@@ -62,8 +52,7 @@ def changeImage(folder, year, filename):
     path = "//home/prosjekt/Histology/"+folder+"/"+year+"/"+filename
     print(path)
     image = openslide.OpenSlide(path)
-    logger.log(25, LogFormat() + current_user.username + " requested image " + filename)
-
+    logger.log(25, HelperClass.LogFormat() + current_user.username + " requested image " + filename)
     deepZoomGen = DeepZoomGenerator(image, tile_size=254, overlap=1, limit_bounds=False)
     return deepZoomGen.get_dzi("jpeg")
 
@@ -72,7 +61,7 @@ def changeImage(folder, year, filename):
 def GetTile(folder, year, dummyVariable, level, tile):
     col, row = GetNumericTileCoordinatesFromString(tile)
     img = deepZoomGen.get_tile(int(level), (int(col), int(row)))
-    return serve_pil_image(img)
+    return HelperClass.serve_pil_image(img)
 
 
 @app.route('/<root>/<imageID>/<file>')
@@ -141,21 +130,13 @@ def GetAvailableImages():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_file("static/images/favicon.ico", mimetype="image/jpeg")
 
-
-
+  
 def GetNumericTileCoordinatesFromString(tile):
     col, row = str.split(tile, "_")
     row = str.replace(row, ".jpeg", "")
     return col, row
-
-
-def serve_pil_image(pil_img):
-    img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=80)
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
 
 
 # User handling methods
@@ -167,14 +148,15 @@ def Login():
         user = User.query.filter_by(username=username).first()
 
         if user is not None and username == user.username and user.check_password(password):
-            logger.log(25, LogFormat() + username + " logged in")
+            logger.log(25, HelperClass.LogFormat() + username + " logged in")
             login_user(user)
             return redirect("/")
         else:
-            logger.log(25, LogFormat() + "Attempted loggin with username: " + username)
+            logger.log(25, HelperClass.LogFormat() + "Attempted loggin with username: " + username)
             return render_template("login.html", type="login", message="Wrong username or password")
 
     return render_template("login.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 #@login_required
@@ -186,10 +168,10 @@ def Register():
         newUser = User(registerUsername, registerPassword)
         db.session.add(newUser)
         db.session.commit()
-        #logger.log(25, LogFormat() + current_user.username + " registered a new user: " + registerUsername)
-        return render_template("login.html")
-    else:
-        return render_template("register.html")
+        logger.log(25, HelperClass.LogFormat() + current_user.username + " registered a new user: " + registerUsername)
+        redirect("/login")
+
+    return render_template("register.html")
 
 
 @login_manager.user_loader
@@ -200,7 +182,7 @@ def user_login(Username):
 @app.route("/logout")
 @login_required
 def Logout():
-    logger.log(25, LogFormat() + current_user.username + " logged out")
+    logger.log(25, HelperClass.LogFormat() + current_user.username + " logged out")
     logout_user()
     return render_template("login.html", type="logout", message = "You are now logged out")
 
@@ -211,30 +193,23 @@ def CatchNotLoggedIn():
     return redirect("/login")
 
 
-def LogFormat():
-    DateTime = str(strftime("%Y-%m-%d %H:%M:%S" , gmtime()))
-    ip = request.remote_addr
-
-    return DateTime + " | " + ip + " | "
-
-## UserMixin contains the standard methods so we dont have to implement it our self
 class User(UserMixin, db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     username = db.Column("Username", db.String,)
     password = db.Column("Password", db.String)
 
-    def __init__(self, usrname, password):
-        self.username = usrname
+    def __init__(self, username, password):
+        self.username = username
         self.password = self.set_password(password)
 
-    def set_password(self, passwrd):
-        return generate_password_hash(passwrd)
+    def set_password(self, password):
+        return generate_password_hash(password)
 
     def get_password(self):
         return self.password
 
-    def check_password(self, passwrd):
-        return check_password_hash(self.password, passwrd)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 if __name__ == '__main__':
