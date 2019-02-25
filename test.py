240af71ade983ca19e9659e428d2bf9a5ac379a6
Ridalor <jacob.ihle@gmail.com>
2019-02-25 09:29:56 +0100

    Deleted vulgar crap

--- test.py
+++
@@ -1,5 +0,0 @@
-import imageList
-
-test = ["//home/prosjekt/test/fuuuuuuu/shit/dritt.scn", "//home/prosjekt/test/fuuuuuuu/shit/ass.scn"]
-foo = imageList.stripBeginningOfPaths(test)
-print(imageList.imageListToDict(foo))
