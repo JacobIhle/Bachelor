ddf566ac0570f54fe4f87f44a6546a00c2c56ee5
TOsaland <thomas.osaland@gmail.com>
2019-03-25 15:17:24 +0100

    h

--- static/GUI.js
+++ static/GUI.js
@@ -74,6 +74,7 @@ function open_slide(url) {
         onRedraw:function(){
             console.log("redraw");
             overlay.canvas().width = overlay.canvas().width;
+            overlay.resize();
             overlay.context2d().strokeStyle = "rgba(255,0,0,1)";
             if(canvasObjects.length > 1) {
                 for(var i=0; i<canvasObjects.length; i++){
@@ -88,7 +89,7 @@ function open_slide(url) {
                 }
             }
         },
-        clearBeforeRedraw:true
+        clearBeforeRedraw:false
     });
 
     $(window).resize(function() {
