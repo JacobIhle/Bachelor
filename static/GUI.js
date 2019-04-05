ae1b3949fa0361d9909b4176cfb6a6d76d39152e
TOsaland <thomas.osaland@gmail.com>
2019-04-05 10:11:56 +0200

    xml

--- static/GUI.js
+++ static/GUI.js
@@ -471,6 +471,7 @@ function generateXML(listOfDrawings) {
         points.forEach(function (point) {
             var vertex = xml.createElement("Vertex");
             vertex.insertAdjacentText("beforebegin", "\n");
+            vertex.textContent = "\n";
             vertex.setAttribute("X", ""+point.x);
             vertex.setAttribute("Y", ""+point.y);
             vertex.setAttribute("Z", "0");
