13f986a38f1b639d8e4504b60b381168729e3ded
Ridalor <jacob.ihle@gmail.com>
2019-04-09 10:09:07 +0200

    added to method for generating html

--- static/GUI.js
+++ static/GUI.js
@@ -376,7 +376,15 @@ function updateAllTags(modifier) {
 
 
 function generateTagSelectorWindow() {
-    var formName = "<form> Name: <input type='text' id='tagName' name='tagName'><br></form>";
+    var formName = "<form> Name: <input type='text' id='tagName' name='tagName'><br>" +
+        "Grade: <select>\n" +
+        "<option>1</option>\n" +
+        "<option>2</option>\n" +
+        "<option>3</option>\n" +
+        "<option>4</option>\n" +
+        "<option>5</option>\n" +
+        "<option>6</option>\n" +
+        "</select></form><br>";
     var plus = "<img src=\"../static/images/plus.svg\" id=\"addSelector\"> <br>";
     var tagsForm = "<form id=\"tagsForm\" method=\"POST\" action=\"/Tags\"><div></div></form>";
 
